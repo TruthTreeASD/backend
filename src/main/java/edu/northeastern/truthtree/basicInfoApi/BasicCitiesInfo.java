3a9e7c0ff4f3c197247cd35840d0830f3b4d6032
applemana <37480204+applemana@users.noreply.github.com>
2019-01-28 20:07:27 -0800

    Feature/basic info api (#2)
    * basic state/county/state api classes
    
    * fixed @RestController on states, cities, and counties basic api classes

---
+++ src/main/java/edu/northeastern/truthtree/basicInfoApi/BasicCitiesInfo.java
@@ -0,0 +1,29 @@
+package edu.northeastern.truthtree.basicInfoApi;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+@RestController
+public class BasicCitiesInfo {
+
+  @RequestMapping("/api/cities")
+  public String getResponse() {
+    JSONParser parser = new JSONParser();
+    JSONArray citiesList = new JSONArray();
+
+    try {
+      citiesList = (JSONArray) parser.parse(new FileReader("./Cities.json"));
+
+    } catch (IOException | ParseException e) {
+      e.printStackTrace();
+    }
+
+    return citiesList.toJSONString();
+  }
+}
---
+++ src/main/java/edu/northeastern/truthtree/basicInfoApi/BasicCountiesInfo.java
@@ -0,0 +1,29 @@
+package edu.northeastern.truthtree.basicInfoApi;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+@RestController
+public class BasicCountiesInfo {
+
+  @RequestMapping("/api/counties")
+  public String getResponse() {
+    JSONParser parser = new JSONParser();
+    JSONArray countiesList = new JSONArray();
+
+    try {
+      countiesList = (JSONArray) parser.parse(new FileReader("./Counties.json"));
+      
+    } catch (IOException | ParseException e) {
+      e.printStackTrace();
+    }
+
+    return countiesList.toJSONString();
+  }
+}
