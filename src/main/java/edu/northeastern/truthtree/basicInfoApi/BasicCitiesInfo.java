96d3ed32b12898561413062b2feada0ac75daf45
Appleman <aaron.appleman@husky.neu.edu>
2019-01-28 19:09:50 -0800

    fixed @RestController on states, cities, and counties basic api classes

--- src/main/java/edu/northeastern/truthtree/basicInfoApi/BasicCitiesInfo.java
+++ src/main/java/edu/northeastern/truthtree/basicInfoApi/BasicCitiesInfo.java
@@ -4,13 +4,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileReader;
 import java.io.IOException;
 
+@RestController
 public class BasicCitiesInfo {
 
-  @RequestMapping("/cities/")
+  @RequestMapping("/api/cities")
   public String getResponse() {
     JSONParser parser = new JSONParser();
     JSONArray citiesList = new JSONArray();
--- src/main/java/edu/northeastern/truthtree/basicInfoApi/BasicStatesInfo.java
+++ src/main/java/edu/northeastern/truthtree/basicInfoApi/BasicStatesInfo.java
@@ -4,11 +4,12 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileReader;
 import java.io.IOException;
 
-
+@RestController
 public class BasicStatesInfo {
 
   @RequestMapping("/api/states")
