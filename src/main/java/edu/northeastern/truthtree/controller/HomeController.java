4b73d62796d3f31379f10a60b1432fa94082e08f
Appleman <aaron.appleman@husky.neu.edu>
2019-02-04 16:18:03 -0800

    Added a landing page with a simple UI

---
+++ src/main/java/edu/northeastern/truthtree/controller/HomeController.java
@@ -0,0 +1,15 @@
+package edu.northeastern.truthtree.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping({"/", "/index"})
+  public String home() {
+
+    return "index";
+  }
+}
+
