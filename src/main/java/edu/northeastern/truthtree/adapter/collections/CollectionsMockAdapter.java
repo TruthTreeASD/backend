4ca8790d294d880d2296c388c825f8b71b798111
Appleman <aaron.appleman@husky.neu.edu>
2019-02-06 10:35:41 -0800

    Utilized the JSONFileReader methods

--- src/main/java/edu/northeastern/truthtree/adapter/collections/CollectionsMockAdapter.java
+++ src/main/java/edu/northeastern/truthtree/adapter/collections/CollectionsMockAdapter.java
@@ -7,21 +7,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import edu.northeastern.truthtree.adapter.utilities.JSONFileReader;
+
 public class CollectionsMockAdapter implements ICollectionsAdapter {
 
 	private static final String COLLECTIONS_FILE_PATH = "src/main/resources/Collections.json";
 
 	@Override
 	public JSONArray getCollections() {
-		JSONParser parser = new JSONParser();
-		JSONArray collections = new JSONArray();
-		try {
-			collections = (JSONArray) parser.parse(new FileReader(COLLECTIONS_FILE_PATH));
-
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-		return collections;
+		return JSONFileReader.readJSONFile(COLLECTIONS_FILE_PATH);
 	}
 
 }
