b8045c3ddb3d4848c09377b5a75114ad0ec0277e
Appleman <aaron.appleman@husky.neu.edu>
2019-02-10 10:39:29 -0800

    Swagger clean up and /error page created

--- src/main/java/edu/northeastern/truthtree/controller/LandingController.java
+++ src/main/java/edu/northeastern/truthtree/controller/LandingController.java
@@ -3,7 +3,10 @@ package edu.northeastern.truthtree.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import springfox.documentation.annotations.ApiIgnore;
+
 @Controller
+@ApiIgnore
 public class LandingController {
 
   @GetMapping({"/", "/index"})
