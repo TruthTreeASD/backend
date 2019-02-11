73497f5eceac8c2d53e5f279bc9918e4ece5c7b2
Neha Shukla <neha91192@gmail.com>
2019-02-11 06:59:50 +0530

    Feature/collections (#22)
    * added changes to get data for collections api
    
    * added annotation for cross origin access
    
    * Update BasicInfo.java
    
    * Update BasicInfo.java
    
    * Update BasicInfo.java

--- src/main/java/edu/northeastern/truthtree/controller/attributes/Attributes.java
+++ src/main/java/edu/northeastern/truthtree/controller/attributes/Attributes.java
@@ -3,6 +3,7 @@ package edu.northeastern.truthtree.controller.attributes;
 import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +11,19 @@ import edu.northeastern.truthtree.service.attributes.IAttributesService;
 
 @RestController
 @Component
-public class Attributes implements IAttributes{
-    private IAttributesService service;
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class Attributes implements IAttributes {
+	private IAttributesService service;
 
-    @Autowired
-    public Attributes(IAttributesService service) {
-        this.service = service;
-    }
+	@Autowired
+	public Attributes(IAttributesService service) {
+		this.service = service;
+	}
 
-    @Override
-    @RequestMapping("/api/attributes")
-    public String getAttributes() {
-        JSONArray response = service.getAttributes();
-        return response.toJSONString();
-    }
+	@Override
+	@RequestMapping("/api/attributes")
+	public String getAttributes() {
+		JSONArray response = service.getAttributes();
+		return response.toJSONString();
+	}
 }
--- src/main/java/edu/northeastern/truthtree/controller/timerange/TimeRange.java
+++ src/main/java/edu/northeastern/truthtree/controller/timerange/TimeRange.java
@@ -1,24 +1,27 @@
 package edu.northeastern.truthtree.controller.timerange;
 
-import edu.northeastern.truthtree.service.timerange.ITimeRangeService;
 import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.northeastern.truthtree.service.timerange.ITimeRangeService;
+
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class TimeRange implements ITimeRange {
-    private ITimeRangeService service;
+	private ITimeRangeService service;
 
-    @Autowired
-    public TimeRange(ITimeRangeService service) {
-        this.service = service;
-    }
+	@Autowired
+	public TimeRange(ITimeRangeService service) {
+		this.service = service;
+	}
 
-    @Override
-    @RequestMapping("/api/time_range")
-    public String getTimeRange() {
-        JSONArray response =  service.getTimeRange();
-        return response.toJSONString();
-    }
+	@Override
+	@RequestMapping("/api/time_range")
+	public String getTimeRange() {
+		JSONArray response = service.getTimeRange();
+		return response.toJSONString();
+	}
 }
