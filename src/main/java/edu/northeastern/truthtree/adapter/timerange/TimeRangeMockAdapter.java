994123aef2c35f920a26848c4bee344edd80e03c
Amit Mangotra <amit_mangotra@outlook.com>
2019-02-19 19:32:39 -0500

    missing import statement in MockAdapter

--- src/main/java/edu/northeastern/truthtree/adapter/timerange/TimeRangeMockAdapter.java
+++ src/main/java/edu/northeastern/truthtree/adapter/timerange/TimeRangeMockAdapter.java
@@ -1,6 +1,7 @@
 package edu.northeastern.truthtree.adapter.timerange;
 
 import edu.northeastern.truthtree.adapter.utilities.JSONUtil;
+import edu.northeastern.truthtree.adapter.utilities.URLUtil;
 import org.json.simple.JSONArray;
 import static edu.northeastern.truthtree.AppConst.TIME_RANGE_FILE_PATH;
 import java.util.List;
