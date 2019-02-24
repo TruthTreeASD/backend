3a5c8eaa31b68b0d1f18cbb4fdb8f0e58659ebe4
Steven Than <steventhan11@gmail.com>
2019-02-23 23:51:58 -0500

    more tabs to spaces

--- src/main/java/edu/northeastern/truthtree/service/basicInfo/IBasicInfoService.java
+++ src/main/java/edu/northeastern/truthtree/service/basicInfo/IBasicInfoService.java
@@ -19,44 +19,44 @@ public interface IBasicInfoService {
    */
   JSONArray getBasicStatesInfo(int[] range);
 
-	/**
-	 * Gets a state information.
-	 *
-	 * @param stateId the state's Id
-	 * @param year the year for population data
-	 * @return a Map optional that contains the state's info if found, otherwise empty optional
-	 */
-	Optional<Map> getStateDetails(String stateId, String year);
+  /**
+   * Gets a state information.
+   *
+   * @param stateId the state's Id
+   * @param year the year for population data
+   * @return a Map optional that contains the state's info if found, otherwise empty optional
+   */
+  Optional<Map> getStateDetails(String stateId, String year);
 
-	/**
-	 * Gets the basic Cities information.
-	 *
-	 * @return basic Cities information as a JSONArray.
-	 */
-	JSONArray getBasicCitiesInfo();
+  /**
+   * Gets the basic Cities information.
+   *
+   * @return basic Cities information as a JSONArray.
+   */
+  JSONArray getBasicCitiesInfo();
 
-	/**
-	 * Gets a state information.
-	 *
-	 * @param cityId the city's Id
-	 * @param year the year for population data
-	 * @return a Map optional that contains the state's info if found, otherwise empty optional
-	 */
-	Optional<Map> getCityDetails(String cityId, String year);
+  /**
+   * Gets a state information.
+   *
+   * @param cityId the city's Id
+   * @param year the year for population data
+   * @return a Map optional that contains the state's info if found, otherwise empty optional
+   */
+  Optional<Map> getCityDetails(String cityId, String year);
 
-	/**
-	 * Gets the basic Cities information.
-	 *
-	 * @return basic Cities information as a JSONArray.
-	 */
-	JSONArray getBasicCountiesInfo();
+  /**
+   * Gets the basic Cities information.
+   *
+   * @return basic Cities information as a JSONArray.
+   */
+  JSONArray getBasicCountiesInfo();
 
-	/**
-	 * Gets a state information.
-	 *
-	 * @param countyId the county's Id
-	 * @param year the year for population data
-	 * @return a Map optional that contains the state's info if found, otherwise empty optional
-	 */
-	Optional<Map> getCountyDetails(String countyId, String year);
+  /**
+   * Gets a state information.
+   *
+   * @param countyId the county's Id
+   * @param year the year for population data
+   * @return a Map optional that contains the state's info if found, otherwise empty optional
+   */
+  Optional<Map> getCountyDetails(String countyId, String year);
 }
