032296a32b922677daccc18659ab02fdee686e5e
peishanwang <wang.peish@husky.neu.edu>
2019-02-24 09:30:58 -0800

    restructure

--- modules/integrationTest/src/main/java/edu/northeastern/truthtree/AttributeApiTests.java
+++ modules/integrationTest/src/main/java/edu/northeastern/truthtree/AttributeApiTests.java
@@ -1,11 +1,9 @@
 package edu.northeastern.truthtree;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Represents the main class for the AttributeApiTests
  */
-@SpringBootApplication
 public class AttributeApiTests {
 
 
