6cbabbf8ec6fc99c40a6b41a23b42f020afeb536
peishanwang <wang.peish@husky.neu.edu>
2019-02-24 09:20:37 -0800

    add modules

---
+++ modules/integrationTest/src/main/java/edu/northeastern/truthtree/AttributeApiTests.java
@@ -0,0 +1,14 @@
+package edu.northeastern.truthtree;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Represents the main class for the AttributeApiTests
+ */
+@SpringBootApplication
+public class AttributeApiTests {
+
+
+
+}
+
