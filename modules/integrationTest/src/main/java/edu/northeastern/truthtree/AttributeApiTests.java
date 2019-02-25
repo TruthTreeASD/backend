041f932aa8c226ecf9e34a371c5363ee50ff5c5b
peishanwang <wang.peish@husky.neu.edu>
2019-02-24 16:30:00 -0800

    add test const

--- modules/integrationTest/src/main/java/edu/northeastern/truthtree/AttributeApiTests.java
+++ modules/integrationTest/src/main/java/edu/northeastern/truthtree/AttributeApiTests.java
@@ -1,12 +1,22 @@
 package edu.northeastern.truthtree;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-/**
- * Represents the main class for the AttributeApiTests
- */
+@SpringBootTest
 public class AttributeApiTests {
 
+  @DataProvider(name = "params")
+  public static Object[][] credentials() {
+    return new Object[][] {
+        { "locationIds=320000000&attributeIds=4&yearList=2000", "Test@123" },
+        { "testuser_1", "Test@123" }};
+  }
 
-
+  @Test(dataProvider = "params")
+  public void testResponse() {
+    System.out.println("hahaha");
+  }
 }
 
---
+++ modules/integrationTest/src/main/java/edu/northeastern/truthtree/TestConst.java
@@ -0,0 +1,10 @@
+package edu.northeastern.truthtree;
+
+public class TestConst {
+  //App Constants
+  private static final String DATABASE_URL = "localhost:8080";
+  private static final String RESOURCES_PATH = "src/main/resources";
+
+  //Attribute Adapter Constants
+  public static final String ATTRIBUTES_API = DATABASE_URL + "/api/attributes?";
+}
