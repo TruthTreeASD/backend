bfc9e9c73a627b733c5fd087a67748b4c1ced08a
Jhalaa <jhalaachinoy@gmail.com>
2019-03-07 00:21:10 -0800

    enabling cors for the normalised api

--- modules/application/src/main/java/edu/northeastern/truthtree/controller/normalization/Normalization.java
+++ modules/application/src/main/java/edu/northeastern/truthtree/controller/normalization/Normalization.java
@@ -3,6 +3,8 @@ package edu.northeastern.truthtree.controller.normalization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.northeastern.truthtree.service.normalization.INormalizationService;
 
 @RestController
+@Component
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class Normalization implements INormalization {
 
   private INormalizationService service;
