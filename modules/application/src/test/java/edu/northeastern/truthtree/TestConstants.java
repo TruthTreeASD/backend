45727e8124a2f72d6507f2141aab50f757a67930
Steven Than <steventhan11@gmail.com>
2019-03-21 00:45:50 -0400

    updated tests

--- modules/application/src/test/java/edu/northeastern/truthtree/TestConstants.java
+++ modules/application/src/test/java/edu/northeastern/truthtree/TestConstants.java
@@ -1,7 +1,7 @@
 package edu.northeastern.truthtree;
 
 public class TestConstants {
-  private static final String RESOURCES_PATH = "src/test/resources";
+  private static final String RESOURCES_PATH = "";
   public static final String ALL_YEARS_GROSS_VALUE = RESOURCES_PATH + "/AttributesForAllYearsForGrossValues.json";
   public static final String YEAR_LIST_GROSS_VALUE = RESOURCES_PATH + "/AttributesForYearListForGrossValues.json";
   public static final String YEAR_RANGE_GROSS_VALUE = RESOURCES_PATH + "/AttributesForYearRangeForGrossValues.json";
