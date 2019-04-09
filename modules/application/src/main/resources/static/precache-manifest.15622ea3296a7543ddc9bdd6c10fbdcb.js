04f78c04f696661b452e56140ddedf415d73d919
Steven Than <steventhan11@gmail.com>
2019-04-08 23:01:30 -0400

    new build

---
+++ modules/application/src/main/resources/static/precache-manifest.15622ea3296a7543ddc9bdd6c10fbdcb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e810ec3dc1b21c6df984",
+    "url": "/static/css/main.a69dc730.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "16c9cae747524046112f",
+    "url": "/static/js/2.16c9cae7.chunk.js"
+  },
+  {
+    "revision": "75b56fb9458a5a002423703cfa0f2350",
+    "url": "/static/media/truthtree-logo.75b56fb9.png"
+  },
+  {
+    "revision": "e810ec3dc1b21c6df984",
+    "url": "/static/js/main.e810ec3d.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "16c9cae747524046112f",
+    "url": "/static/css/2.3cf80c2a.chunk.css"
+  },
+  {
+    "revision": "98c762554c6d51e5051d367a07253677",
+    "url": "/index.html"
+  }
+];
