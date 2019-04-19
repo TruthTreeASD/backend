bf400f52c3ac5a98d19331c8e3bdd75d3b120320
peishanwang <35388523+peishanwang@users.noreply.github.com>
2019-04-18 21:50:12 -0700

    fixed integration tests (#100)

--- modules/integrationTest/src/main/java/edu/northeastern/truthtree/BasicInfoApiTests.java
+++ modules/integrationTest/src/main/java/edu/northeastern/truthtree/BasicInfoApiTests.java
@@ -1,35 +1,34 @@
 package edu.northeastern.truthtree;
 
-import org.json.simple.JSONArray;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
 import static edu.northeastern.truthtree.TestConst.BASIC_INFO_API;
 import static edu.northeastern.truthtree.TestConst.RESOURCES_PATH;
 import static edu.northeastern.truthtree.Utilities.readJSONFile;
 import static edu.northeastern.truthtree.Utilities.readJSONFromURL;
-import static org.testng.Assert.assertEquals;
+
+import org.json.JSONException;
+import org.json.simple.JSONArray;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 
 @SpringBootTest
 public class BasicInfoApiTests {
 
-    @DataProvider(name = "data")
-    public static Object[][] credentials() {
-        return new Object[][]{
-                //{"states", "basicinfo/allStates.json"},
-                //{"states/80000000", "basicinfo/stateById.json"},
-                //{"cities", "basicinfo/allCities.json"},
-                {"cities/262060004", "basicinfo/cityById.json"},
-                //{"counties", "basicinfo/allCounties.json"},
-                {"counties/391013013", "basicinfo/countyById.json"}};
-    }
+  @DataProvider(name = "data")
+  public static Object[][] credentials() {
+    return new Object[][]{
+        {"states", "basicinfo/allStates.json"},
+        {"states/80000000", "basicinfo/stateById.json"},
+        {"cities/262060004", "basicinfo/cityById.json"},
+        {"counties/391013013", "basicinfo/countyById.json"}};
+  }
 
-    @Test(dataProvider = "data")
-    public void testResponse(String params, String expectedResponsePath) {
-        JSONArray actualResponse = readJSONFromURL(BASIC_INFO_API + params);
-        JSONArray expectedResponse = readJSONFile(RESOURCES_PATH + expectedResponsePath);
-        assertEquals(actualResponse, expectedResponse);
-    }
+  @Test(dataProvider = "data")
+  public void testResponse(String params, String expectedResponsePath) throws JSONException {
+    JSONArray actualResponse = readJSONFromURL(BASIC_INFO_API + params);
+    JSONArray expectedResponse = readJSONFile(RESOURCES_PATH + expectedResponsePath);
+    JSONAssert.assertEquals(expectedResponse.toJSONString(), actualResponse.toJSONString(), false);
+  }
 }
--- modules/integrationTest/src/main/java/edu/northeastern/truthtree/CollectionApiTests.java
+++ modules/integrationTest/src/main/java/edu/northeastern/truthtree/CollectionApiTests.java
@@ -1,35 +1,33 @@
 package edu.northeastern.truthtree;
 
+import static edu.northeastern.truthtree.TestConst.COLLECTIONS_API;
+import static edu.northeastern.truthtree.TestConst.RESOURCES_PATH;
+import static edu.northeastern.truthtree.Utilities.readJSONFile;
+import static edu.northeastern.truthtree.Utilities.readJSONFromURL;
+
+import org.json.JSONException;
+import org.json.simple.JSONArray;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
-import static edu.northeastern.truthtree.Utilities.readJSONFile;
-import static edu.northeastern.truthtree.Utilities.readJSONFromURL;
-import static org.testng.Assert.assertEquals;
-
-import org.json.simple.JSONArray;
-
-import static edu.northeastern.truthtree.TestConst.COLLECTIONS_API;
-import static edu.northeastern.truthtree.TestConst.RESOURCES_PATH;
-
 @SpringBootTest
 public class CollectionApiTests {
 
-    @DataProvider(name = "data")
-    public static Object[][] credentials() {
-        return new Object[][]{
-                //{"", "collection/allCollections.json"},
-                {"?locationId=182056011", "collection/collectionByLocationId1.json"},
-                {"?locationId=320000000", "collection/collectionByLocationId2.json"}};
-    }
+  @DataProvider(name = "data")
+  public static Object[][] credentials() {
+    return new Object[][]{
+        {"", "collection/allCollections.json"},
+        {"?locationId=182056011", "collection/collectionByLocationId1.json"},
+        {"?locationId=320000000", "collection/collectionByLocationId2.json"}};
+  }
 
-    @Test(dataProvider = "data")
-    public void testResponse(String params, String expectedResponsePath) {
-        JSONArray actualResponse = readJSONFromURL(COLLECTIONS_API + params);
-        JSONArray expectedResponse = readJSONFile(RESOURCES_PATH + expectedResponsePath);
-        assertEquals(actualResponse, expectedResponse);
-    }
+  @Test(dataProvider = "data")
+  public void testResponse(String params, String expectedResponsePath) throws JSONException {
+    JSONArray actualResponse = readJSONFromURL(COLLECTIONS_API + params);
+    JSONArray expectedResponse = readJSONFile(RESOURCES_PATH + expectedResponsePath);
+    JSONAssert.assertEquals(expectedResponse.toJSONString(), actualResponse.toJSONString(), false);
+  }
 }
 
